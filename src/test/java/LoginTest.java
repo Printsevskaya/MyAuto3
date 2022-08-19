64aa85910232a1471a24e7d3ef5b0dbf5107c5e8
Printsevskaya <helloman005@gmail.com>
2022-08-19 17:55:07 +0300

    2Commit

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,16 +1,20 @@
+import org.example.ConfProperties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
 
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
 
+    @BeforeClass
     public static void setup(){
         WebDriver driver = new ChromeDriver();
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\julia\\OneDrive\\Рабочий стол\\ChromeDriver");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\julia\\OneDrive\\ChromeDriver");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("http://passport.yandex.ru/auth");
+       // driver.get("http://passport.yandex.ru/auth");
+        driver.get(ConfProperties.getProperty("loginpage"));
 
 
     }
