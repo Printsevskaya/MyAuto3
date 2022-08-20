84f25d47de026bb239b1b50952b27a28d3bf5ffa
Printsevskaya <helloman005@gmail.com>
2022-08-20 14:28:58 +0300

    4Commit

--- src/test/java/org/example/LoginPage.java
+++ src/test/java/org/example/LoginPage.java
@@ -1,9 +1,38 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-    @FindBy(xpath = "//*[@id=\"passp-field-login\"]")
+
+    public WebDriver driver;
+    public LoginPage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    @FindBy(xpath = "//*[contains(@id, 'passp-field-login')]")
     private WebElement loginfield;
+
+    @FindBy(xpath = "//*[contains(text(), 'Войти')]")
+    private WebElement loginBtn;
+
+    @FindBy(xpath = "//*[contains(@id, 'passp-field-passwd')]")
+    private WebElement passwdField;
+
+    public void inputLogin(String login){
+        loginfield.sendKeys(login);
+    }
+
+    public void inputPasswd(String passwd){
+        loginfield.sendKeys(passwd);
+    }
+
+    public void clickLoginBtn(){
+        loginBtn.click();
+    }
+
+
 }
