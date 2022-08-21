91381633f5705af35bca663d8b62afeab06d0f51
Printsevskaya <helloman005@gmail.com>
2022-08-21 17:59:03 +0300

    5Commit

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,4 +1,7 @@
 import org.example.ConfProperties;
+import org.example.LoginPage;
+import org.example.ProfilePage;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
@@ -15,7 +18,18 @@ public class LoginTest {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
        // driver.get("http://passport.yandex.ru/auth");
         driver.get(ConfProperties.getProperty("loginpage"));
+        loginPage = new LoginPage(driver);
+        profilePage = new ProfilePage(driver);
+        driver = new ChromeDriver();
+    }
 
+    public static LoginPage loginPage;
+    public static ProfilePage profilePage;
+    public static WebDriver driver;
+
+
+    @Test
+    public void loginTest(){
 
     }
 
